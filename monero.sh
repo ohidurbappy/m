b349b26f958bcd19893dc0fa9ede6cf25cae3c97
Ohidur Rahman Bappy <22405409+ohidurbappy@users.noreply.github.com>
2023-07-07 11:09:24 +0600

    Create monero.sh

---
+++ monero.sh
@@ -0,0 +1,9 @@
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+mkdir build
+cd build
+cmake ..
+make
